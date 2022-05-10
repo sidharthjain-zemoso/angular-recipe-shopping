44d0aa1f98f63f41d0d42e173ec6c27f933a00c3
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-11 00:24:00 +0530

    added child routes and configured route parameters

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 import { Recipe } from "../../recipe.model";
 import { RecipeService } from "../../recipe.service";
@@ -14,8 +14,4 @@ export class RecipeItemComponent implements OnInit {
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit() {}
-
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
 }
