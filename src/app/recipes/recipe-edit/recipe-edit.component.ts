43d3dede9a7f4799cda2fc1b1b751db5c5f54b35
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-13 02:19:39 +0530

    syncing html with form and added ingredient controls to form array

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { NgForm } from "@angular/forms";
+import { FormArray, FormControl, FormGroup, NgForm } from "@angular/forms";
 import { ActivatedRoute, Params } from "@angular/router";
+import { RecipeService } from "../recipe.service";
 
 @Component({
   selector: "app-recipe-edit",
@@ -10,13 +11,48 @@ import { ActivatedRoute, Params } from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode: boolean;
-  @ViewChild("f") recipeForm: NgForm;
-  constructor(private route: ActivatedRoute) {}
+  recipeForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.id = params["id"];
       this.editMode = params["id"] != null;
+      this.initForm();
+    });
+  }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImagePath = "";
+    let recipeDescription = "";
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe["ingredients"]) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              name: new FormControl(ingredient.name),
+              amount: new FormControl(ingredient.amount),
+            })
+          );
+        }
+      }
+    }
+    this.recipeForm = new FormGroup({
+      name: new FormControl(recipeName),
+      imagePath: new FormControl(recipeImagePath),
+      description: new FormControl(recipeDescription),
+      ingredients: recipeIngredients,
     });
   }
 
@@ -30,9 +66,11 @@ export class RecipeEditComponent implements OnInit {
 
   onCancel() {}
 
-  onSubmit() {}
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
 
   getRecipeIngredientsControls() {
-    return [];
+    return (<FormArray>this.recipeForm.get("ingredients")).controls;
   }
 }
