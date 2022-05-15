9db7b86e70c8a399024eefa224b1fa25133ff53b
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-15 18:07:20 +0530

    switch between auth modes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  selector: "app-auth",
+  templateUrl: "./auth.component.html",
+  styleUrls: ["./auth.component.css"],
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
   }
-
 }
