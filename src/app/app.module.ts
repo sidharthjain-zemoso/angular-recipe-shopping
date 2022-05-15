6219c584f21ebde808fb9c3e8c11853c591b7da8
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 04:17:11 +0530

    understanding the core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AuthInterceptorService } from "./auth/auth-interceptor.service";
 import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
+import { CoreModule } from "./core.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, AuthComponent],
@@ -29,13 +30,7 @@ import { SharedModule } from "./shared/shared.module";
     ShoppingListModule,
     SharedModule,
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
-  ],
+  providers: [CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
