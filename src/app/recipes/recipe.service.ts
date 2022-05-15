7365a9d45a9dc2b488986bfca282dd901046cdc2
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-15 17:23:21 +0530

    fetching recipes, tranforming response data and resolving data before loading

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,20 +10,22 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
   recipeSelected = new Subject<Recipe>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Dosa",
-      "Tasty Dosa",
-      "https://www.vegrecipesofindia.com/wp-content/uploads/2021/07/dosa-recipe-1.jpg",
-      [new Ingredient("Rawa", 1), new Ingredient("Oil", 1)]
-    ),
-    new Recipe(
-      "Idli",
-      "Tasty Idli",
-      "https://i0.wp.com/cookingfromheart.com/wp-content/uploads/2016/08/Idli-5.jpg",
-      [new Ingredient("Rawa", 1), new Ingredient("Oil", 1)]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Dosa",
+  //     "Tasty Dosa",
+  //     "https://www.vegrecipesofindia.com/wp-content/uploads/2021/07/dosa-recipe-1.jpg",
+  //     [new Ingredient("Rawa", 1), new Ingredient("Oil", 1)]
+  //   ),
+  //   new Recipe(
+  //     "Idli",
+  //     "Tasty Idli",
+  //     "https://i0.wp.com/cookingfromheart.com/wp-content/uploads/2016/08/Idli-5.jpg",
+  //     [new Ingredient("Rawa", 1), new Ingredient("Oil", 1)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
