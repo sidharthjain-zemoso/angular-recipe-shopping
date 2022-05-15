62ad1ae6371db7b595f074970ab5ecb08b4a801e
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 03:19:06 +0530

    splitting modules correctly

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -6,6 +6,8 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -16,7 +18,14 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
     RecipeStartComponent,
     RecipeEditComponent,
   ],
-  imports: [CommonModule],
-  exports: [RecipesModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
 })
 export class RecipesModule {}
