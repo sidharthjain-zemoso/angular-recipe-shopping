6ba4e120a17fb8c9d6f6873774a29285c9f5a5c8
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-15 18:44:29 +0530

    preparing the signup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+interface AuthResponseData {
+  idToken: string; // A Firebase Auth ID token for the newly created user.
+  email: string; // The email for the newly created user.
+  refreshToken: string; //	A Firebase Auth refresh token for the newly created user.
+  expiresIn: string; //	The number of seconds in which the ID token expires.
+  localId: string; // The uid of the newly created user.
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBuS9sQFbCWnFQAVbMIZjskrVy1YmZ3ENo",
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      }
+    );
+  }
+}
