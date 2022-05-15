c9e4f6e3b195b8215f23052aebc2dd7c182e0867
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 01:16:53 +0530

    reflecting the auth state in ui

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthResponseData, AuthService } from "./auth.service";
 
@@ -13,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
@@ -41,6 +42,7 @@ export class AuthComponent implements OnInit {
       next: (resData) => {
         console.log(resData);
         this.isLoading = false;
+        this.router.navigate(["/recipes"]);
       },
       error: (e) => {
         console.log(e);
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { throwError } from "rxjs";
+import { Subject, throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { User } from "./user.model";
+import { tap } from "rxjs";
 
 export interface AuthResponseData {
   idToken: string; // A Firebase Auth ID token for the newly created user.
@@ -9,6 +11,7 @@ export interface AuthResponseData {
   refreshToken: string; //	A Firebase Auth refresh token for the newly created user.
   expiresIn: string; //	The number of seconds in which the ID token expires.
   localId: string; // The uid of the newly created user.
+  registered: boolean; // Whether the email is for an existing account.
 }
 
 @Injectable({
@@ -16,6 +19,7 @@ export interface AuthResponseData {
 })
 export class AuthService {
   firebaseApiKey = "AIzaSyBuS9sQFbCWnFQAVbMIZjskrVy1YmZ3ENo";
+  user = new Subject<User>();
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -32,24 +36,65 @@ export class AuthService {
         }
       )
       .pipe(
-        catchError((errorRes) => {
-          let errorMessage = "An unknown error occurred!";
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMessage));
-          }
-          switch (errorRes.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage = "This email exists already";
-          }
-          return throwError(() => new Error(errorMessage));
+        catchError((errorRes) => this.handleError(errorRes)),
+        tap((responseData: AuthResponseData) => {
+          this.handleAuthentication(
+            responseData.email,
+            responseData.localId,
+            responseData.idToken,
+            +responseData.expiresIn
+          );
         })
       );
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.firebaseApiKey}`,
-      { email: email, password: password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.firebaseApiKey}`,
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(catchError((errorRes) => this.handleError(errorRes)))
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occurred!";
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email exists already";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "This email does not exits.";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "This password is not correct.";
+        break;
+    }
+    return throwError(() => new Error(errorMessage));
   }
 }
