6a87e58637a57b98e21f1828bfbe7284e7625360
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 02:46:04 +0530

    protecting routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGaurd } from "./auth/auth-gaurd.service";
 import { AuthComponent } from "./auth/auth.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
@@ -13,6 +14,7 @@ const appRoutes: Routes = [
   {
     path: "recipes",
     component: RecipesComponent,
+    canActivate: [AuthGaurd],
     children: [
       { path: "", component: RecipeStartComponent },
       { path: "new", component: RecipeEditComponent },
