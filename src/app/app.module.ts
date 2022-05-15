2c9510934f6ed3834ec9d40c3a33d01f9c522b45
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 04:25:44 +0530

    adding a auth feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,21 +16,22 @@ import { RecipesModule } from "./recipes/recipes.module";
 import { ShoppingListModule } from "./shopping-list/shopping-list.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
+import { AuthModule } from "./auth/auth.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
 
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [CoreModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
