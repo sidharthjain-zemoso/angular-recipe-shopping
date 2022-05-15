e19a921a5013eac77825ee4728872b12cb95cee8
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-15 12:06:21 +0530

    storing recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-  constructor() { }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        "https://angular-course-project-ad1b1-default-rtdb.asia-southeast1.firebasedatabase.app/recipes.json",
+        recipes
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
