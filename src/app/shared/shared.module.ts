09c090292d1d5c2c359b6ff32f329497f15f0ac4
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 22:37:44 +0530

    adding an alert modal component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
 import { DropdownDirective } from "./dropdown.directive";
+import { AlertComponent } from "./alert/alert.component";
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent, DropdownDirective],
+  declarations: [LoadingSpinnerComponent, DropdownDirective, AlertComponent],
   imports: [CommonModule],
-  exports: [LoadingSpinnerComponent, DropdownDirective, CommonModule],
+  exports: [
+    LoadingSpinnerComponent,
+    DropdownDirective,
+    CommonModule,
+    AlertComponent,
+  ],
 })
 export class SharedModule {}
