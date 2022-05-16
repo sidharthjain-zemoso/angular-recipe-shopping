70e68f0b2f74004f9d8ecbc13953451ca1109c3f
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-16 10:04:58 +0530

    using environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIKey: "AIzaSyBuS9sQFbCWnFQAVbMIZjskrVy1YmZ3ENo",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIKey: "AIzaSyBuS9sQFbCWnFQAVbMIZjskrVy1YmZ3ENo",
 };
 
 /*
