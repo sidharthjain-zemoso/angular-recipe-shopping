ed56cda0890281ea41333dfdc2bdee72d50b9231
Sidharth Jain <sidharth.jain@zemosolabs.com>
2022-05-17 08:31:12 +0530

    facing a blocker  Cannot read properties of undefined (reading 'viewContainerRef')

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  ComponentFactoryResolver,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { Observable, Subscription } from "rxjs";
+import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 import { AuthResponseData, AuthService } from "./auth.service";
 
 @Component({
@@ -9,12 +17,25 @@ import { AuthResponseData, AuthService } from "./auth.service";
   templateUrl: "./auth.component.html",
   styleUrls: ["./auth.component.css"],
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   isLoginMode = true;
   isLoading = false;
   error: string = null;
+  @ViewChild(PlaceholderDirective, { static: false })
+  alertHost: PlaceholderDirective;
+  private closeSub: Subscription;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
+  }
 
   ngOnInit(): void {}
 
@@ -47,6 +68,7 @@ export class AuthComponent implements OnInit {
       error: (e) => {
         console.log(e);
         this.error = e;
+        this.showErrorAlert(e);
         this.isLoading = false;
       },
     });
@@ -55,5 +77,21 @@ export class AuthComponent implements OnInit {
 
   onHandleError() {
     this.error = null;
+  }
+
+  private showErrorAlert(message: string) {
+    // const alertCmp = new AlertComponent();
+    const alertCmpFactory =
+      this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+
+    componentRef.instance.message = message;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+    });
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from "@angular/common";
 import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
 import { DropdownDirective } from "./dropdown.directive";
 import { AlertComponent } from "./alert/alert.component";
+import { PlaceholderDirective } from "./placeholder/placeholder.directive";
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent, DropdownDirective, AlertComponent],
+  declarations: [
+    LoadingSpinnerComponent,
+    DropdownDirective,
+    AlertComponent,
+    PlaceholderDirective,
+  ],
   imports: [CommonModule],
   exports: [
     LoadingSpinnerComponent,
     DropdownDirective,
     CommonModule,
     AlertComponent,
+    PlaceholderDirective,
   ],
+  entryComponents: [AlertComponent],
 })
 export class SharedModule {}
